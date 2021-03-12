da4a5bae6c523bcf7140c9c9e20a58033ab9cb49
skho14 <samkhosrow@gmail.com>
2021-03-12 11:30:03 +0100

    import des notions php

---
+++ TP2/ci.php
@@ -0,0 +1,25 @@
+<?php
+require_once('template_header.php');
+?>
+
+
+<html>
+    <head>
+        <meta charset="utf-8">
+    </head>
+    <body>
+        <h1>Centre d'intérêts</h1>
+        <nav class="menu">
+            <ul>
+            <li><a href="index.html">Accueil</a></li>
+            <li><a href="cv.html">CV</a></li>
+            <li><a href="ci.html">Centre d'intérêts</a></li>
+            </ul>
+        </nav>
+        <article>
+            <section>Sport</section>
+            <section>Management</section>
+            <section>Voyage</section>
+        </article>
+    </body>
+</html>
