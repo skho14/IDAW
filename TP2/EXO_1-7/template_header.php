6fd40e19df91d552199d732df5cc446854e9352d
skho14 <samkhosrow@gmail.com>
2021-03-15 12:31:04 +0100

    partie 6-7

--- TP2/EXO_1-7/template_header.php
+++ TP2/EXO_1-7/template_header.php
@@ -4,12 +4,12 @@
 <head>
 <div class="header">
   <h1>Site Pro Sam </h1>
-  <p>Voici le résumé de mon parcours professionel</p>
+  
 </div>
 
 <div class="topnav">
   <a href="#">CV</a>
-  <a href="#">LinkedIn</a>
+  <a href="https://www.linkedin.com/in/sam-khosrowshahi-a70953190/">LinkedIn</a>
   
 </div>
 <link rel="stylesheet" href="style1.css" type="text/css"
