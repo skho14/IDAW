eeee0de78a20ef7fb8a7c933e8a7e2a3fb3e86c5
skho14 <samkhosrow@gmail.com>
2021-03-15 11:17:59 +0100

    Update index.php

--- TP2/EXO_1-5/index.php
+++ TP2/EXO_1-5/index.php
@@ -1,26 +1,26 @@
 <?php
-require_once("header.php");
-require_once("menu.php");
-$currentPageId = 'accueil';
-if(isset($_GET['page'])) {
-$currentPageId = $_GET['page'];
-}
+  require_once("template_header.php");
+  require_once("template_menu.php");
+  $currentPageId = 'accueil';
+  if(isset($_GET['page'])) {
+    $currentPageId = $_GET['page'];
+  }
 ?>
 <header class="bandeau_haut">
 <h1 class="titre">Sam KHOSROWSHAHI</h1>
 </header>
 <?php
-renderMenuToHTML($currentPageId);
+  renderMenuToHTML($currentPageId);
 ?>
 <section class="corps">
 <?php
-$pageToInclude = $currentPageId . ".php";
-if(is_readable($pageToInclude))
-require_once($pageToInclude);
-else
-require_once("error.php");
+  $pageToInclude = $currentPageId . ".php";
+  if(is_readable($pageToInclude))
+    require_once($pageToInclude);
+  else
+    require_once("error.php");
 ?>
 </section>
 <?php
-require_once("footer.php");
+  require_once("template_footer.php");
 ?>
