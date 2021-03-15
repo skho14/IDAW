6b65b6d7910a8f94bb09a83117235316d7b2dfe7
skho14 <samkhosrow@gmail.com>
2021-03-15 10:16:08 +0100

    Update ci.php

--- TP2/EXO_1-5/ci.php
+++ TP2/EXO_1-5/ci.php
@@ -9,13 +9,10 @@ require_once('template_header.php');
     </head>
     <body>
         <h1>Centre d'intérêts</h1>
-        <nav class="menu">
-            <ul>
-            <li><a href="index.html">Accueil</a></li>
-            <li><a href="cv.html">CV</a></li>
-            <li><a href="ci.html">Centre d'intérêts</a></li>
-            </ul>
-        </nav>
+        <?php
+require_once('template_menu.php');
+renderMenuToHTML('index');
+?>
         <article>
             <section>Sport</section>
             <section>Management</section>
